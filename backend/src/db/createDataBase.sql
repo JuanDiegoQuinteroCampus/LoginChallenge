4a3fd1c81eb7eebb2950a7240152033fb6e712d7
JuanDiegoQuinteroCampus <juanquinterocampu33@gmail.com>
2025-03-15 12:09:36 -0500

    feat: :sparkles: Created other sql to implement

--- backend/src/db/createDataBase.sql
+++ backend/src/db/createDataBase.sql
@@ -1,6 +1,4 @@
-CREATE DATABASE loginChallenge;
-
-USE loginChallenge;
+USE b6vlma4zt58z9kkb60qq;
 
 CREATE TABLE persons (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -15,7 +13,16 @@ CREATE TABLE users (
     username VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     id_person INT NOT NULL,
-    last_login TIMESTAMP NULL,
+    last_login TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (id_person) REFERENCES persons(id) ON DELETE CASCADE
 );
 
+INSERT INTO persons (name, lastname, email, cellphone_number)
+VALUES 
+  ('Juan Diego', 'Quintero', 'juandiegoquintero2505@gmail.com', '3000000000'),
+  ('jktic', 'soluciones', 'jktic.soluciones@gmail.coom', '3100000000');
+
+INSERT INTO users (username, password, id_person)
+VALUES 
+  ('juandiego', '$2a$10$ZhuN0fvKklZF9.97SQjS1edRfAZGHifD6Qw0CNzIKXvPB6SM6JLie', 1),
+  ('jktic', '$2a$10$hY28G7WNfGeywIshUjqoo.pOwXojxQaGhNIgnFZf45J3Tx1IU5Dn2', 2);
