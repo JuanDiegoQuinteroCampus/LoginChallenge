5ec4af167126f81e41e95c208b69d19bc4a9664c
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2025-03-16 04:13:22 -0500

    feat: :ambulance: Configuration to add headers and receive a parameter by post method

--- backend/src/main.php
+++ backend/src/main.php
@@ -3,6 +3,11 @@
 require_once __DIR__ . '/config/Connect.php';
 require_once __DIR__ . '/controllers/UserController.php';
 
-$action = isset($_GET['action']) ? intval($_GET['action']) : 0;
+header("Access-Control-Allow-Origin: *"); // Permitir todas las conexiones
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS"); // Métodos permitidos
+header("Access-Control-Allow-Headers: Content-Type, Authorization"); // Encabezados permitidos
+
+
+$action = isset($_POST['action']) ? intval($_POST['action']) : 0;
 
 new UserController($action);
